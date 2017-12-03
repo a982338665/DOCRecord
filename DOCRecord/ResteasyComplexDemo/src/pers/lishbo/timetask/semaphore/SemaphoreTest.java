0040264c9eb2d6e4c81d8943cd686fb2c9b07aee
lishengbo <luofengli123@163.com>
2017-12-03 16:02:38 +0800

    控制并发线程对象学习

---
+++ DOCRecord/ResteasyComplexDemo/src/pers/lishbo/timetask/semaphore/SemaphoreTest.java
@@ -0,0 +1,17 @@
+package pers.lishbo.timetask.semaphore;
+/**
+ * semphore可以控制当前访问自身线程的个数，并提供同步机制。---等同于队列
+ * 
+ * 控制同时访问资源的线程个数，实现一个文件允许并发的访问数
+ * 
+ * 例：
+ * 一个接口，最多控制为10个线程可以同时访问，进入接口的线程可以是随机的，也可以是按先后顺序的，这取决于构造此对象
+ * 时传入的参数
+ * 
+ * @author lishengbo
+ * @Time   2017年12月3日下午3:55:36
+ *
+ */
+public class SemaphoreTest {
+
+}
